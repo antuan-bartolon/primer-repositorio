ab515c30d4ac84e5edc0f7effce79fa670efeec6
BVAJ <BVAJ@DESKTOP-K7ERQ70>
2024-04-20 23:00:48 -0500

    puse un sout

--- src/controlador/Main.java
+++ src/controlador/Main.java
@@ -3,6 +3,7 @@ package controlador;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("LLAMAR AL LOGIN");
        Controlador.mostrarLogin();
      }
     
